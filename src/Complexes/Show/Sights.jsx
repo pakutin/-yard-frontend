03041be4e1044966d872bd9da11f6f7c10149e14
Nikita Pakutin <nikita@pakutin.ru>
2017-08-05 18:01:21 +0500

    Fixes for shadows

--- src/Complexes/Show/Sights.jsx
+++ src/Complexes/Show/Sights.jsx
@@ -14,20 +14,15 @@ const Sights = styled.section`
   `};
 `;
 
-const MapWrapper = styled.div`
-  box-shadow: 0 0 1.25rem 0.25rem rgba(0, 0, 0, 0.3);
+const MapCard = styled.div`
   ${media.md`
     padding-right: 1rem;
   `};
 `;
 
-const SightList = styled.div`
-  background-color: #fff;
-  box-shadow: 0 0 1.25rem 0.25rem rgba(0, 0, 0, 0.3);
-  ${media.md`
-    padding-left: 1rem;
-  `};
-`;
+const SightCard = styled.div`background-color: #fff;`;
+
+const Shadow = styled.div`box-shadow: 0 0 1.25rem 0.25rem rgba(0, 0, 0, 0.3);`;
 
 type Props = { location: LocationType };
 
@@ -35,16 +30,20 @@ export default (props: Props) =>
   (<Sights>
     <div className="altfbg">
       <div className="altfbg-row">
-        <MapWrapper className="altfbg-col6-xs altfbg-col6-md">
-          {props.location.latitude &&
-            props.location.longitude &&
-            <Map latitude={props.location.latitude} longitude={props.location.longitude} />}
-        </MapWrapper>
-        <SightList className="altfbg-col6-xs altfbg-col6-md">
-          <Sight name="Красный Октябрь" distance="24 минуты, 6 км" />
-          <Sight name="World Class" distance="2 минуты, 300 м" />
-          <Sight name="Третьяковская галерея" distance="14 минут, 4 км" />
-        </SightList>
+        <MapCard className="altfbg-col6-xs altfbg-col6-md">
+          <Shadow>
+            {props.location.latitude &&
+              props.location.longitude &&
+              <Map latitude={props.location.latitude} longitude={props.location.longitude} />}
+          </Shadow>
+        </MapCard>
+        <SightCard className="altfbg-col6-xs altfbg-col6-md">
+          <Shadow>
+            <Sight name="Красный Октябрь" distance="24 минуты, 6 км" />
+            <Sight name="World Class" distance="2 минуты, 300 м" />
+            <Sight name="Третьяковская галерея" distance="14 минут, 4 км" />
+          </Shadow>
+        </SightCard>
       </div>
     </div>
   </Sights>);
